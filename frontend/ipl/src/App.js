161103980b7869f8477210b163221e955760ec43
raw-at <abhinavrawat2308@gmail.com>
2018-05-10 09:07:18 +0530

    front-end started

---
+++ frontend/ipl/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Home from './containers/Home/home';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      
+          <Home />
+      
+      </div>
+    );
+  }
+}
+
+export default App;
